6518e17bfa51fe631afc8e558a1f772b57c60c91
Konstantinos <kongallis@gmail.com>
2020-07-18 17:03:41 +0300

    Initial commit

---
+++ service-registration-and-discovery-client/src/test/java/com/example/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistrationanddiscoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationAndDiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
